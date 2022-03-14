6df3cd6b99269ca847e9d09488c75e6d5c6b8181
David Pitutiu <56295878+davidpitutiu@users.noreply.github.com>
2022-03-14 10:18:30 +0200

    Added navbar to the profile and logout function

--- links/profile.php
+++ links/profile.php
@@ -10,9 +10,21 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+  <link rel="stylesheet" href="style.css">
   <title>Profile</title>
 </head>
 <body>
-  <p>OKISH</p>
+  <nav class="navbar navbar-dark bg-dark">
+    <?php
+      $sql = "SELECT firstname FROM users where user_id = '$user_id'";
+      $result = mysqli_query($connect, $sql);
+      while ($row = $result->fetch_assoc()) {
+						$firstname = $row['firstname'];
+			}
+    ?>
+     <a class="navbar-brand" href="profile.php"><?php echo $firstname; ?></a>
+     <a class="navbar-brand" href="logout.php">Log Out</a>
+  </nav>
 </body>
 </html>
